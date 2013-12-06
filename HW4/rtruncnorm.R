914a4f9f74611c0ad47a6362023443f3faf5d09b
Stephanie Chan <stephanietchan@gmail.com>
2013-12-05 17:02:46 -0800

    start truncnorm using R

---
+++ HW4/rtruncnorm.R
@@ -0,0 +1,8 @@
+rtruncnormR = function(N, mu, sigma, lo, hi, mu_len, sigma_len, lo_len, hi_len, 
+	      maxtries, nullnum, rng_a, rng_b, rng_c) 
+{
+	rnorm(N,mu,sigma)	
+}
+
+truncnorm_single = function(mu,sigma,lo,hi,maxtries) {
+    
